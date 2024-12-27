835f6cf9941f96dc739163730a0830c39bd2e3cc
Rys-Nowak <ryszard.nowak012@gmail.com>
2024-12-27 22:56:06 +0100

    cropper

--- clouds-app/src/app/pic-loader/pic-loader.component.ts
+++ clouds-app/src/app/pic-loader/pic-loader.component.ts
@@ -1,6 +1,10 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 import { ApiService } from '../shared/services/api.service';
 import { Prediction } from '../../prediction';
+import Cropper from 'cropperjs';
+
+import 'cropperjs/dist/cropper.css';
+
 
 @Component({
   selector: 'app-pic-loader',
@@ -14,6 +18,11 @@ export class PicLoaderComponent {
   predictions: Prediction[] = []
   @Output() predictionResult = new EventEmitter<Prediction[]>();
   loading: boolean = false;
+  cropper: Cropper | null = null;
+  selectedImage: string | null = null;
+
+  @ViewChild('cropperModal') cropperModal!: ElementRef;
+  @ViewChild('imageElement') imageElement!: ElementRef<HTMLImageElement>;
 
   constructor(private apiService: ApiService) { }
 
@@ -23,11 +32,48 @@ export class PicLoaderComponent {
       Array.from(files).forEach((file) => {
         const reader = new FileReader();
         reader.onload = (e: any) => {
-          this.uploadedImages.push(e.target.result);
+          this.selectedImage = e.target.result;
+          this.showCropper();
         };
         reader.readAsDataURL(file as File);
       });
     }
+  }
+
+  showCropper(): void {
+    const modal = this.cropperModal.nativeElement;
+    modal.style.display = 'flex';
+    setTimeout(() => {
+      if (this.imageElement) {
+        this.cropper = new Cropper(this.imageElement.nativeElement, {
+          aspectRatio: 1,
+          viewMode: 1,
+          autoCropArea: 1,
+          responsive: true,
+          background: false,
+          zoomable: true,
+        });
+      }
+    }, 0);
+  }
+
+  cropAndSave(): void {
+    if (this.cropper) {
+      const croppedCanvas = this.cropper.getCroppedCanvas();
+      const croppedImage = croppedCanvas.toDataURL('image/jpeg');
+      this.uploadedImages.push(croppedImage);
+      this.closeCropper();
+    }
+  }
+
+  closeCropper(): void {
+    const modal = this.cropperModal.nativeElement;
+    modal.style.display = 'none';
+    if (this.cropper) {
+      this.cropper.destroy();
+      this.cropper = null;
+    }
+    this.selectedImage = null;
   }
 
   async submitImages(): Promise<void> {
